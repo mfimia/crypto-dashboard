7e91413bc90be79dceaa204798224698e71fb1da
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-12 22:02:13 +0100

    app component using crypto context and rendering crypto list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import CryptoList from "./components/crypto/CryptoList";
+import CryptoState from "./context/CryptoState";
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CryptoState>
+      <CryptoList />
+    </CryptoState>
   );
-}
+};
 
 export default App;
